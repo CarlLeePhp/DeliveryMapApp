2cdfe934f8144415f919df3f71685ba5e6231691
Carl Li <leekunhui@sina.cn>
2020-08-02 19:43:58 +1200

    testCase

--- DeliveryMapTest/Models/XeroAPITests.cs
+++ DeliveryMapTest/Models/XeroAPITests.cs
@@ -22,14 +22,24 @@ namespace DeliveryMapTest.Models
         {
             double weight = XeroAPI.GetWeight("Apple Pie 23kg");
             Assert.AreEqual(23, weight);
-            weight = XeroAPI.GetWeight("Apple Pie 23 kg");
-            Assert.AreEqual(23, weight);
-            weight = XeroAPI.GetWeight("Apple Pie 23 kg new");
-            Assert.AreEqual(23, weight);
+            weight = XeroAPI.GetWeight("Apple Pie 24 kg");
+            Assert.AreEqual(24, weight);
+            weight = XeroAPI.GetWeight("Apple Pie 25 kg new");
+            Assert.AreEqual(25, weight);
 
             // failed cases
-            //weight = XeroAPI.GetWeight("Apple Packge 23 kg new");
-            //Assert.AreEqual(23, weight);
+            //weight = XeroAPI.GetWeight("Apple Package 26-kg) new");
+            //Assert.AreEqual(26, weight);
+            //weight = XeroAPI.GetWeight("Apple Packge 27 kg new");
+            //Assert.AreEqual(27, weight);
+        }
+
+        [Test]
+        public void testK()
+        {
+            string sub1 = "20 kg()";
+            string result = sub1.Substring(0, sub1.IndexOf("kg")+2);
+            Assert.AreEqual("20 kg", result);
         }
     }
 }
